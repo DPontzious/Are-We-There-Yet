600ceed13c44235c0df1c840bdced44d97343c12
Danelle Pontzious <danellepontzious@Danelles-MacBook-Air.local>
2019-03-20 13:17:40 -0700

    added struct

---
+++ app/src/components/Jumbo/Jumbo.js
@@ -0,0 +1,8 @@
+function Jumbo() {
+    render(
+        <div class="jumbotron">
+            <h1 class="display-4">Hello, world!</h1>
+        </div>
+    )
+}
+export default Jumbo;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+// const mongoose = require("mongoose");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,10 +11,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
