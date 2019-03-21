5692e068b7fccb564fdc7f16bce574b5af547218
Daniel Mata <danielmata@Daniels-MacBook-Pro.local>
2019-03-21 11:44:49 -0700

    login

---
+++ Register.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import './Login.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import {Motion, spring} from 'react-motion';
+import NavigationPanel from './components/Login/NavigationPanel';
+import Modal from './components/Login/Modal';
+
+class Register extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			mounted: false
+		};
+	}
+
+	componentDidMount() {
+		this.setState({ mounted: true });
+	}
+	
+	handleSubmit = (e) => {
+		this.setState({ mounted: false });
+		e.preventDefault();
+	}
+
+	render() {
+		const {mounted} = this.state;
+
+		let child;
+		let test = 12;
+
+		if(mounted) {
+			child = (
+				<div className="App_test">
+					<NavigationPanel></NavigationPanel>
+					<Modal onSubmit={this.handleSubmit}/>
+				</div>
+			);
+		}
+		
+		return(
+			<div className="App">
+				<ReactCSSTransitionGroup 
+					transitionName="example"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={300}>
+						{child}
+				</ReactCSSTransitionGroup>
+			</div>
+		);
+	}
+}
+
+export default Register;
--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Register from './Register';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, <Register />, document.getElementById("root"));
