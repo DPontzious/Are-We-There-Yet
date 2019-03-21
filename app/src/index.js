eb6800862d3faeb5fd3bf5a736d3720f486bd280
Daniel Mata <danielmata@Daniels-MacBook-Pro.local>
2019-03-21 12:00:16 -0700

    changes

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Register from './Register';
+import Register from './Register.js';
 
 ReactDOM.render(<App />, <Register />, document.getElementById("root"));
