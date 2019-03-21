038d2a70b05a92fce41732d277c11f76dbe50c89
Baruch Hashem <barukhashem@gmail.com>
2019-03-21 13:02:32 -0700

    Routes working.

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Register from './Register';
 
-ReactDOM.render(<App />, <Register />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ app/src/pages/Trip/Trip.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Trip() {
+    return (
+        <div>
+            <h1>Hello, testing Trip</h1>
+        </div>
+
+    )
+}
+export default Trip;
