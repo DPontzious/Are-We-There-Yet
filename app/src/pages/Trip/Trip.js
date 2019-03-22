699b428964eda585d17eccdfb88f8f78e7f916d7
Danelle Pontzious <danellepontzious@Danelles-MacBook-Air.local>
2019-03-22 10:59:17 -0700

    merge

--- app/src/pages/Trip/Trip.js
+++ app/src/pages/Trip/Trip.js
@@ -1,56 +1,50 @@
 
-import { React, Component } from "react";
+// import { React, Component } from "react";
+// import Gas from "../../components/Forms/Input"
+// import { format } from "url";
 
+// class Trip extends Component(props) {
 
-
-class Trip extends Component(props) {
-
-    handleFormSubmit() {
+//     handleFormSubmit() {
         // api bing zip search
         // then update map
-    }
-    componentDidMount() {
-        var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
-        var location = new Microsoft.Maps.Location(35.0278, -111.0222);
-        var pushpin = new Microsoft.Maps.Pushpin(location, { color: 'green' });
-        var zipLocation;
+    // }
+    // componentDidMount() {
+    //     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+    //     var location = new Microsoft.Maps.Location(35.0278, -111.0222);
+    //     var pushpin = new Microsoft.Maps.Pushpin(location, { color: 'green' });
+    //     var zipLocation;
 
-        //push the pin onto the map
-        map.entities.push(pushpin);
+    //push the pin onto the map
+    // map.entities.push(pushpin);
 
-        //the setView function is a bing maps function for setting the center of the map view
-        map.setView({
-            //changes the type of map
-            mapTypeId: Microsoft.Maps.MapTypeId.aerial,
-            //sets the center
-            center: new Microsoft.Maps.Location(35.027222, -111.0225),
-            //sets the zoom
-            zoom: 15
-        });
+    //the setView function is a bing maps function for setting the center of the map view
+    // map.setView({
+    //changes the type of map
+    // mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+    //sets the center
+    // center: new Microsoft.Maps.Location(35.027222, -111.0225),
+    //sets the zoom
+    // zoom: 15
+    // });
+    // }
+//     render() {
+//         <div>
+//             <div id="myMap"></div>
+//             <form>
+//                 {/* more code */}
+//                 <button onClick={() => handleFormSubmit()}></button>
+//             </form>
+//         </div>
 
-    }
-
-    render() {
-        <div>
-            <div id="myMap"></div>
-            <form>
-                {/* more code */}
-                <button onClick={() => handleFormSubmit()}></button>
-            </form>
-            {/* everything else: form (zip), table, etc. */}
-import Gas from "../../components/Forms/Input"
-import { format } from "url";
-function Trip() {
-    return (
-        <div>
-            <Gas components={Gas} />
-            <h1>Hello, testing Trip</h1>
-        </div>
-    }
-};
+//         return (
+//             <div>
+//                 <Gas components={Gas} />
+//                 <h1>Hello, testing Trip</h1>
+//             </div >
+//         )
+//     }
+// };
 
 
-    )
-}
-
-export default Trip;
+// export default Trip;
