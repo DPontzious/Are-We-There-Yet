bd0c4a1c75a790ee7694129e5ac0dfe532e6cf1e
Baruch Hashem <barukhashem@gmail.com>
2019-03-22 12:44:26 -0700

    adding others' code

--- app/src/pages/Trip/Trip.js
+++ app/src/pages/Trip/Trip.js
@@ -1,5 +1,5 @@
-import { React, Component } from "react";
-
+import React, { Component } from "react";
+import { ReactBingmaps } from "react-bingmaps";
 
 
 class Trip extends Component {
@@ -9,36 +9,18 @@ class Trip extends Component {
         // then update map
     }
     componentDidMount() {
-        var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
-        var location = new Microsoft.Maps.Location(35.0278, -111.0222);
-        var pushpin = new Microsoft.Maps.Pushpin(location, { color: 'green' });
-        var zipLocation;
-
-        //push the pin onto the map
-        map.entities.push(pushpin);
-
-        //the setView function is a bing maps function for setting the center of the map view
-        map.setView({
-            //changes the type of map
-            mapTypeId: Microsoft.Maps.MapTypeId.aerial,
-            //sets the center
-            center: new Microsoft.Maps.Location(35.027222, -111.0225),
-            //sets the zoom
-            zoom: 15
-        });
-
     }
 
     render() {
         return (
             <div>
-                <div id="myMap" style='position:relative;width:600px;height:400px;'></div>
+                <ReactBingmaps bingmapKey="AswFsvLf2w5DotjCEdVZ8m8KpOrZ41ADV4r43PDIMcknbmlhVUhPv2B8amujy5Gq"></ReactBingmaps>
+                {/* <div id="myMap" style='position:relative;width:600px;height:400px;'></div> */}
                 <form>
-                    {/* more code */}
-                    <button onClick={() => this.handleFormSubmit()}></button>
+
                 </form>
                 {/* everything else: form (zip), table, etc. */}
-            </div>
+            </div >
         )
     }
 };
