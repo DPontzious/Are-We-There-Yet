2c0f735d4f2b2df326a63a7ba97b7ccf440c5e6e
Baruch Hashem <barukhashem@gmail.com>
2019-03-28 12:04:15 -0700

    adding map functionality + directions

--- app/src/components/Forms/Input.js
+++ app/src/components/Forms/Input.js
@@ -1,34 +1,6 @@
 import React from 'react';
 import { Form, FormGroup, Input } from 'reactstrap';
 import "./style.css";
-<<<<<<< HEAD
-class input extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { start: "", destination: "" };
-    };
-
-
-
-    handleChange = name => event => {
-        this.setState({
-            [name]: event.target.value,
-        });
-    };
-
-    render() {
-        return (
-            <Form>
-                <FormGroup>
-                    <Input
-                        type={this.props.type}
-                        name={this.props.name}
-                        id={this.props.id}
-                        placeholder={this.props.placeholder} />
-                </FormGroup>
-            </Form>
-        );
-=======
 
 class input extends React.Component
 {
@@ -58,7 +30,6 @@ class input extends React.Component
             </FormGroup>
              </Form>
             );
->>>>>>> ed2b8a083cf031971bd20b1ffa6e13facab3b34f
     }
 }
 export default input;
