43a3e76dc84f411fc4bd1795ee3c3f8f5ce36ae6
Danelle Pontzious <danellepontzious@Danelles-MacBook-Air.local>
2019-03-29 10:20:13 -0700

    style

--- app/src/pages/Events/index.js
+++ app/src/pages/Events/index.js
@@ -37,7 +37,7 @@ class Events extends Component {
         console.log("helllooooo!")
         axios.get("v1/events")
             .then(res => {
-                console.log(res.data.events.event, "data");
+                // console.log(res.data.events.event, "data");
                 this.setState({ resultEvent: res.data.events.event });
             })
             .catch(err => console.log(err))
@@ -48,21 +48,35 @@ class Events extends Component {
                 <Col xs="1" />
                 <Col xs="2" >
                     <Toogle1
-                        rules={"Categories.One person picks a category (ex: Britney Spear’s songs, NFL teams, flavors of La Croix) and everyone takes turns naming something in that category until someone (the loser) is stumped."} />
+                        gameName={"Categories"}
+                        rules={"One person picks a category (ex: Britney Spear’s songs, NFL teams, flavors of La Croix) and everyone takes turns naming something in that category until someone (the loser) is stumped."} />
                     <Toogle1
-                        rules={"Alphabet. Take turns going through the alphabet. Each player must find the next letter either on something in the car (like the stereo screen) or license plates, or road signs."} />
+                        gameName={"Alphabet"}
+                        rules={". Take turns going through the alphabet. Each player must find the next letter either on something in the car (like the stereo screen) or license plates, or road signs."} />
                     <Toogle1
-                        rules={"Guess the song. Turn the radio on or put your Spotify/CD player on shuffle. Whoever can shout out the name of the song first, wins. (Be sure to cover you your radio screen if it says the song names as they play)."} />
+                        gameName={"Guess the song"}
+                        rules={" Turn the radio on or put your Spotify/CD player on shuffle. Whoever can shout out the name of the song first, wins. (Be sure to cover you your radio screen if it says the song names as they play)."} />
                     <Toogle1
-                        rules={"Truth or Car-Contained Dare. A game of truth or dare where all the dares must be something that can be accomplished (safely) in the car."} />
+                        gameName={"Truth or Car"}
+                        rules={"A game of truth or dare where all the dares must be something that can be accomplished (safely) in the car."} />
                     <Toogle1
-                        rules={"Deep Questions. Find a long list of good questions to get to know someone and work your way through them. You’ll learn a lot about everyone in the car."} />
+                        gameName={"Deep Questions"}
+                        rules={"Find a long list of good questions to get to know someone and work your way through them. You’ll learn a lot about everyone in the car."} />
                     <Toogle1
-                        rules={"Going on a Picnic. This is a story memory game where someone says “I’m going on a picnic and I’m going to bring….” and then lists an item. The next person begins I’m going on a picnic and I’m going to bring….” and must list the first person’s item before adding their own item. The list grows and grows and the first person to not be able to correctly list all the items is the loser (or out, if you have enough people to play in elimination rounds)."} />
+                        gameName={"Going on a Picnic"}
+                        rules={"This is a story memory game where someone says “I’m going on a picnic and I’m going to bring….” and then lists an item. The next person begins I’m going on a picnic and I’m going to bring….” and must list the first person’s item before adding their own item. The list grows and grows and the first person to not be able to correctly list all the items is the loser (or out, if you have enough people to play in elimination rounds)."} />
                     <Toogle1
-                        rules={"I Spy. The road trip classic. One person says “I spy with my little eye…. something” and then vaguely describes something they see, like “I spy something red”. The other players take turn asking yes or no questions until they can guess what the item is."} />
+                        gameName={"I Spy"}
+                        rules={"The road trip classic. One person says “I spy with my little eye…. something” and then vaguely describes something they see, like “I spy something red”. The other players take turn asking yes or no questions until they can guess what the item is."} />
                     <Toogle1
-                        rules={""} />
+                        gameName={"Story"}
+                        rules={"One person says a word that begins a story. Take turns adding one word onto the story and see where it ends up!"} />
+                    <Toogle1
+                        gameName={"The License Plate Game"}
+                        rules={"Interpret the letters in each license plate you pass. For instance, REG could stand for “Ron eats garbage” and CSB could be “Claire smells bad”."} />
+                    <Toogle1
+                        gameName={"Would You Rather"}
+                        rules={"Play an epic game of Would You Rather. Try to stump the other person with the weirdest or most difficult questions you can come up with (or find online)."} />
                 </Col>
                 <Col xs="6" className="back">
                     {this.state.resultEvent.map((event, key) =>
